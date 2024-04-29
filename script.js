fe54e23712e15a79436c997e6108524048c5cd64
maBrn <165158255+emmasbrrrN@users.noreply.github.com>
2024-04-29 14:41:05 +0200

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+function getData() {
+   fetch('data.json')
+     .then((response) => {
+       if (!response.ok) {
+         throw new Error('Network response was not ok');
+       }
+       return response.json();
+     })
+     .then((data) => {
+       // Traitez les données comme vous le souhaitez
+       console.log('Données récupérées du fichier JSON :', data);
+       /// ON ECRIT LE CODE ICI ! 
+      
+let nomJournal = data.journal.nomJournal
+
+console.log(nomJournal);
+      
+
+       /// FIN DU CODE
+     })
+     .catch((error) => console.error('Erreur lors de la lecture des données :', error));
+ }
+ 
+ getData();
+
+ ///ON écrit les fonctions ici
